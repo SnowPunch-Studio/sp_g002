43a07c2dc168f604cdf883b396336714c4985796
Ahmed Boulad <ahmedboulad95@gmail.com>
2020-02-25 20:36:25 -0600

    Created base PlayerState class and derived classes for each of the four attack states

--- Assets/Scripts/CombatController.cs
+++ Assets/Scripts/CombatController.cs
@@ -4,46 +4,76 @@ using UnityEngine;
 
 public class CombatController : MonoBehaviour
 {
+    [SerializeField] private Animator playerAnim;
+
     public Camera cam;
+    private Utility util;
+
+    private PlayerState state;
+
+    /*enum PlayerState { 
+        IDLE_UP,
+        IDLE_DOWN,
+        IDLE_LEFT,
+        IDLE_RIGHT,       
+        ATTACK_UP, 
+        ATTACK_DOWN, 
+        ATTACK_LEFT, 
+        ATTACK_RIGHT,
+        DEFENSE_UP,
+        DEFENSE_DOWN,
+        DEFENSE_LEFT,
+        DEFENSE_RIGHT,
+        STUN_UP,
+        STUN_DOWN,
+        STUN_LEFT,
+        STUN_RIGHT,
+        MINING_UP,
+        MINING_DOWN,
+        MINING_LEFT,
+        MINING_RIGHT,
+        PLANT_UP,
+        PLANT_DOWN,
+        PLANT_LEFT,
+        PLANT_RIGHT,
+        TEND_UP,
+        TEND_DOWN,
+        TEND_LEFT,
+        TEND_RIGHT,
+        HARVEST_UP,
+        HARVEST_DOWN,
+        HARVEST_LEFT,
+        HARVEST_RIGHT 
+    }*/
 
     // Start is called before the first frame update
     void Start()
     {
-
+        util = new Utility();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        switch(util.GetInputDirection())
         {
-            Vector3 cursorPosition = cam.ScreenToWorldPoint(Input.mousePosition);
-
-            float adjustedX = cursorPosition.x - transform.position.x;
-            float adjustedY = cursorPosition.y - transform.position.y;
-
-            if (Mathf.Abs(adjustedX) > Mathf.Abs(adjustedY))
-            {
-                if (adjustedX > 0)
-                {
-                    Debug.Log("Right");
-                }
-                else
-                {
-                    Debug.Log("Left");
-                }
-            }
-            else
-            {
-                if (adjustedY > 0)
-                {
-                    Debug.Log("Up");
-                }
-                else
-                {
-                    Debug.Log("down");
-                }
-            }
+            case Utility.Direction.UP:
+                state = new SAttackUp();
+                break;
+            case Utility.Direction.DOWN:
+                state = new SAttackDown();
+                break;
+            case Utility.Direction.LEFT:
+                state = new SAttackLeft();
+                break;
+            case Utility.Direction.RIGHT:
+                state = new SAttackRight();
+                break;
+        }
+        if(state != null)
+        {
+            state.ProcessState();
+            state = null;
         }
     }
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility
+{
+    private Camera mainCamera;
+    private GameObject player;
+    public enum Direction { NULL, UP, DOWN, LEFT, RIGHT }
+
+    public Utility()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    public Direction GetInputDirection()
+    {
+
+
+        Direction inputDirection = Direction.NULL;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 cursorPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            float adjustedX = cursorPosition.x - player.transform.position.x;
+            float adjustedY = cursorPosition.y - player.transform.position.y;
+
+            if (Mathf.Abs(adjustedX) > Mathf.Abs(adjustedY))
+            {
+                if (adjustedX > 0)
+                {
+                    //Debug.Log("Right");
+                    inputDirection = Direction.RIGHT;
+                }
+                else
+                {
+                    //Debug.Log("Left");
+                    inputDirection = Direction.LEFT;
+                }
+            }
+            else
+            {
+                if (adjustedY > 0)
+                {
+                    //Debug.Log("Up");
+                    inputDirection = Direction.UP;
+                }
+                else
+                {
+                    //Debug.Log("down");
+                    inputDirection = Direction.DOWN;
+                }
+            }
+        }
+        return inputDirection;
+    }
+}
